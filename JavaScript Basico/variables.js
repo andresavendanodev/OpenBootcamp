09c2d49abb72781d9185dcd965a76a913a231912
andresavendanodev <jordres1@gmail.com>
2022-11-07 19:45:19 -0500

    JAVASCRIPT ejercicio 2 - tipos de datos

---
+++ JavaScript Basico/variables.js
@@ -0,0 +1,6 @@
+let nombre = "Jorge";
+let edad = 25;
+let desarrollador = true;
+let fecha = new Date('Sep 09 1997');
+
+console.log(nombre, " - ", edad, " - ", desarrollador, " - ", fecha.toDateString())
